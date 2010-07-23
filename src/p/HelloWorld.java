ae4fbaef53a527f3731183cce0bedcf15dbf1dbb
exmachina <jonelund@gmail.com>
2010-07-23 09:37:05 +0200

    Improved main method
    HelloWorld main changed to include "Hello Git!"

--- src/p/HelloWorld.java
+++ src/p/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld
 	 * @param args
 	 */
 	public static void main (String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Hello Git");
 
 	}
 
