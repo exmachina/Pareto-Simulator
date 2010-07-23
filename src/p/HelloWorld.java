546d95e56c5fde0a084f10587ab64cda6dec211c
exmachina <jonelund@gmail.com>
2010-07-23 20:55:29 +0200

    Backed to the version without hello git
    Hello World is enough

--- src/p/HelloWorld.java
+++ src/p/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld
 	 * @param args
 	 */
 	public static void main (String[] args) {
-		System.out.println("Hello World! Hello Git");
+		System.out.println("Hello World!");
 
 	}
 
