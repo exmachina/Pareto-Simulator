a54deed86d45d600d1ecd15f67c14510e01da1d2
exmachina <jonelund@gmail.com>
2010-07-23 09:33:32 +0200

    HelloWorld application
    Added first version of main
    
    Signed-off-by: exmachina <jonelund@gmail.com>

---
+++ src/p/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package p;
+
+/**
+ * @author jonedl
+ * 
+ */
+public class HelloWorld
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
