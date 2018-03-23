70db5ddc2fe7e61234ad274f515438c016bd06b5
Jonas Edlund <jonelund@gmail.com>
2018-03-23 14:39:17 +0100

    Added Calc.java

---
+++ src/p1/Calc.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		Calc c = new Calc();
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("" + i + " " + c.calculate(i));
+		}
+	}
+
+	private int calculate(int i) {
+		return 2 * i;
+	}
+}
